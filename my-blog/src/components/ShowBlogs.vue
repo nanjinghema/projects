ca9413c3f16122fd32edb66e4641049479435a46
nanjinghema <dxkxiaofeiyang08@163.com>
2019-06-10 11:42:51 +0800

    the first time

---
+++ my-blog/src/components/ShowBlogs.vue
@@ -0,0 +1,67 @@
+<template>
+  <div  v-theme:column="'narrow'" id="show-blogs">
+   	<h1>博客总览</h1>
+   	<input type="text" name="" v-model="search" placeholder="搜索">
+   	<div v-for="blog in filteredBlogs" class="single-blog">
+   		<h2 v-rainbow>{{blog.title | to-uppercase}}</h2>
+   		<article>{{blog.body | snippet}}</article>
+   	</div>
+  </div>
+</template>
+
+<script>
+
+
+	export default {
+	  name: 'show-blogs',
+	  data(){
+	  	return {
+	  		blogs:[],
+	  		search:""
+	  	}
+	  },
+	  created(){
+	  	// this.$http.get("https://jsonplaceholder.typicode.com/posts")
+	  	this.$http.get("./../static/posts.json")
+	  	.then(function(data){
+	  		this.blogs = data.body.slice(0,10);
+	  		console.log(this.blogs);
+	  	})
+	  },
+	  computed:{
+	  	filteredBlogs:function(){
+	  		return this.blogs.filter((blog) => {
+	  			return blog.title.match(this.search);
+	  		})
+	  	}
+	  },
+	  filters:{
+	  	"to-uppercase":function(value){
+	  		return value.toUpperCase();
+	  	},
+	  	"snippet":function(value){
+	  		return value.slice(0,100) + "..."
+	  	}
+	  },
+	  directives:{
+	  	'rainbow':{
+	  		bind(el,binding,vnode){
+	  			el.style.color = "#" + Math.random().toString(16).slice(2,8);
+	  		}
+	  	}
+	  }
+	}
+</script>
+
+<style>
+	#show-blogs {
+		max-width: 800px;
+		margin: 0 auto;
+	}
+	.single-blog{
+		padding: 20px;
+		margin: 20px;
+		box-sizing: border-box;
+		background: #eee;
+	}
+</style>
---
+++ my-blog/src/routes.js
@@ -0,0 +1,8 @@
+import ShowBlogs from './components/ShowBlogs'
+import AddBlog from './components/AddBlog'
+
+export default[
+	{path:"/",component:ShowBlogs},
+	{path:"/add",component:AddBlog}
+	
+]
